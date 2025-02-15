19a6d7185ce733934560089da3eac97a3b56137f
NIGHTFURY60 <jeswinchristie2@gmail.com>
2025-02-15 23:52:32 +0530

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ from io import BytesIO
 # Flask app setup
 app = Flask(__name__)
 app.config['MAX_CONTENT_LENGTH'] = 100 * 1024 * 1024
-CORS(app, resources={r"/*": {"origins": "http://127.0.0.1:5500"}})
+CORS(app, resources={r"/*": {"origins": "https://travel-trove-main.onrender.com"}})
 
 
 
